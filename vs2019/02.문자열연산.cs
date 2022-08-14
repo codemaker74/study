1f14aad1f0a37a6711b840cd0e9b6d0223b4b1a4
codemaker74 <67043015+codemaker74@users.noreply.github.com>
2022-08-14 17:36:56 +0900

    Create 02.문자열연산.cs

---
+++ vs2019/02.문자열연산.cs
@@ -0,0 +1,24 @@
+using System; 
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //실행 => Ctrl + F5
+
+
+            string a = "hello";
+            string b = "h";
+
+            b += "ello";
+            Console.WriteLine(a == b);
+            Console.WriteLine("b = " + b);
+
+            int x = 10;
+            string c = b + '!' + " " + x;
+            Console.WriteLine("c = " + c);
+        }
+    }
+}
