e157d6bd7f060ba9b4f24792002304dda41c8adb
codemaker74 <67043015+codemaker74@users.noreply.github.com>
2022-08-14 17:18:13 +0900

    Create 01.console.cs

---
+++ vs2019/01.console.cs
@@ -0,0 +1,20 @@
+using System; 
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //실행 => Ctrl + F5
+            Console.Write("Hello ");
+            Console.WriteLine("World!");
+            Console.Write("이름을 입력하세요: ");
+
+            string name = Console.ReadLine();
+            Console.Write("안녕하세요, ");
+            Console.Write(name);
+            Console.WriteLine("님!");
+        }
+    }
+}
