f9d0d1976bd419e388ce28232739d0152cc4cb75
codemaker74 <67043015+codemaker74@users.noreply.github.com>
2022-09-17 12:17:06 +0900

    ES6 기본 사용법

---
+++ react/es6/App.js
@@ -0,0 +1,30 @@
+import logo from './logo.svg';
+import './App.css'; 
+import R009_Es6 from './R009_Es6';
+import R010_Variable from './R010_Variable';
+import R011_SpreadOperator from './R011_SpreadOperator';
+import R012_Class from './R012_Class';
+import R013_ArrrowFunction from './R013_ArrrowFunction';
+import R014_ForEach from './R014_ForEach';
+import R015_Map from './R015_Map';
+import R016_Jquery from './R016_Jquery';
+
+function App() {
+  return (
+    <div>
+      <h1> Start React 200! </h1> 
+      {/*
+      <R009_Es6></R009_Es6>
+      <R010_Variable></R010_Variable>
+      <R011_SpreadOperator></R011_SpreadOperator>
+      <R012_Class></R012_Class>
+      <R013_ArrrowFunction></R013_ArrrowFunction>
+      <R014_ForEach></R014_ForEach>
+      <R015_Map></R015_Map>
+      */}
+      <R016_Jquery></R016_Jquery>
+    </div>    
+  );
+}
+
+export default App;
