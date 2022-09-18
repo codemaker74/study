b4b3caebcad8e8bfafe830acb514e931ccc927a3
codemaker74 <67043015+codemaker74@users.noreply.github.com>
2022-09-18 11:17:20 +0900

    프론트엔드 디자인을 쉽게 구현할수있게 도와주는 html, css, js 프레임워크

---
+++ react/reactstrap/App.js
@@ -0,0 +1,69 @@
+import logo from './logo.svg';
+//import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import R034_ReactstrapAlerts from './R034_ReactstrapAlerts';
+import R035_ReactstrapBadges from './R035_ReactstrapBadges';
+import R036_ReactstrapBreadcrumbs from './R036_ReactstrapBreadcrumbs';
+import R037_ReactstrapDropdown from './R037_ReactstrapDropdown';
+import R038_ReactstrapButtonGroup from './R038_ReactstrapButtonGroup';
+import R039_ReactstrapBottons from './R039_ReactstrapBottons';
+import R040_ReactstrapCard from './R040_ReactstrapCard';
+import R041_ReactstrapCarousel from './R041_ReactstrapCarousel';
+import R042_ReactstrapCollapse from'./R042_ReactstrapCollapse';
+import R043_ReactstrapFade from './R043_ReactstrapFade';
+import R044_ReactstrapForm from './R044_ReactstrapForm';
+//import R045_ReactstrapInputGroup from'./R045_ReactstrapInputGroup';
+//import R046_ReactstrapJumbotron from './R046_ReactstrapJumbotron';
+import R047_ReactstrapListGroup from './R047_ReactstrapListGroup';
+import R048_ReactstrapModal from './R048_ReactstrapModal';
+import R049_ReactstrapNavbar from './R049_ReactstrapNavbar';
+import R050_ReactstrapPagination from './R050_ReactstrapPagination';
+import R051_ReactstrapPopover from './R051_ReactstrapPopover';
+import R052_ReactstrapProgress from './R052_ReactstrapProgress';
+import R053_ReactstrapSpinner from './R053_ReactstrapSpinner';
+import R054_ReactstrapTable from './R054_ReactstrapTable';
+import R055_ReactstrapTab from './R055_ReactstrapTab';
+import R056_Sweetalert2Basic from './R056_Sweetalert2Basic';
+import R057_Sweetalert2Position from './R057_Sweetalert2Position';
+import R058_Sweetalert2Confirm from './R058_Sweetalert2Confirm';
+
+function App() {
+  return (
+    <div>
+      <h1> Start React 200! </h1> 
+      {/*
+      <R034_ReactstrapAlerts></R034_ReactstrapAlerts>
+      <R035_ReactstrapBadges></R035_ReactstrapBadges>
+      <R036_ReactstrapBreadcrumbs></R036_ReactstrapBreadcrumbs>
+      <R037_ReactstrapDropdown></R037_ReactstrapDropdown>
+      <R038_ReactstrapButtonGroup></R038_ReactstrapButtonGroup>
+      <R039_ReactstrapBottons></R039_ReactstrapBottons>
+      <R040_ReactstrapCard></R040_ReactstrapCard>
+      <R041_ReactstrapCarousel></R041_ReactstrapCarousel>
+      <R042_ReactstrapCollapse></R042_ReactstrapCollapse>
+      <R043_ReactstrapFade></R043_ReactstrapFade>
+      <R044_ReactstrapForm></R044_ReactstrapForm>
+      
+      참고) https://gallery-k.tistory.com/250
+      <R045_ReactstrapInputGroup></R045_ReactstrapInputGroup> 에러! 버전 차이인듯...
+      <R046_ReactstrapJumbotron></R046_ReactstrapJumbotron> 에러! 버전 차이인듯...
+      <R047_ReactstrapListGroup></R047_ReactstrapListGroup>
+      <R048_ReactstrapModal></R048_ReactstrapModal>
+      <R049_ReactstrapNavbar></R049_ReactstrapNavbar>
+      <R050_ReactstrapPagination></R050_ReactstrapPagination>
+      <R051_ReactstrapPopover></R051_ReactstrapPopover>
+      <R052_ReactstrapProgress></R052_ReactstrapProgress>
+      <R053_ReactstrapSpinner></R053_ReactstrapSpinner>
+      <R054_ReactstrapTable></R054_ReactstrapTable>
+      <R055_ReactstrapTab></R055_ReactstrapTab>
+      <R056_Sweetalert2Basic></R056_Sweetalert2Basic>
+      <R057_Sweetalert2Position></R057_Sweetalert2Position>
+      */} 
+      
+      <R058_Sweetalert2Confirm></R058_Sweetalert2Confirm>
+
+    </div>    
+  );
+}
+
+export default App;
